c960475e46a04ad4bf965fef4f69603f0619c56b
Garrett Luu <garrettluu131@gmail.com>
2018-06-20 22:57:06 -0700

    Added smoothState animations, wrote third entry, wrote about page

--- scripts/main.js
+++ scripts/main.js
@@ -1,110 +1,93 @@
-let imageZoom1 = document.getElementById('entry1');
-let imageZoom2 = document.getElementById('entry2');
+stupid = function() {
+    let imageZoom1 = document.getElementById('entry1');
+    let imageZoom2 = document.getElementById('entry2');
+    let imageZoom3 = document.getElementById('entry3');
 
-let img1 = document.getElementById('img1');
-let zoomContent1 = document.getElementById('img01');
+    let img1 = document.getElementById('img1');
+    let zoomContent1 = document.getElementById('img01');
 
-let img2 = document.getElementById('img2');
-let zoomContent2 = document.getElementById('img02');
+    let img2 = document.getElementById('img2');
+    let zoomContent2 = document.getElementById('img02');
 
-img1.onclick = function() {
-    imageZoom1.style.display = "block";
-    zoomContent1.src = this.src;
-};
+    let img3 = document.getElementById('img3');
+    let zoomContent3 = document.getElementById('img03');
 
-imageZoom1.onclick = function() {
-    imageZoom1.classList.add('fadeaway');
-    imageZoom1.addEventListener("animationend", listener, false);
 
-};
+    img1.onclick = function () {
+        imageZoom1.style.display = "block";
+        zoomContent1.src = this.src;
+    };
 
-img2.onclick = function() {
-    imageZoom2.style.display = "block";
-    zoomContent2.src = this.src;
-};
+    imageZoom1.onclick = function () {
+        imageZoom1.classList.add('fadeaway');
+        imageZoom1.addEventListener("animationend", listener, false);
 
-imageZoom2.onclick = function() {
-    imageZoom2.classList.add('fadeaway');
-    imageZoom2.addEventListener("animationend", listener, false);
+    };
 
-};
+    img2.onclick = function () {
+        imageZoom2.style.display = "block";
+        zoomContent2.src = this.src;
+    };
 
-function listener(event) {
-    if (event.type === "animationend"){
-        imageZoom1.style.display = "none";
-        imageZoom1.classList.remove('fadeaway');
-        imageZoom1.removeEventListener("animationend", listener, false);
-        imageZoom2.style.display = "none";
-        imageZoom2.classList.remove('fadeaway');
-        imageZoom2.removeEventListener("animationend", listener, false);
-    }
-}
+    imageZoom2.onclick = function () {
+        imageZoom2.classList.add('fadeaway');
+        imageZoom2.addEventListener("animationend", listener, false);
 
-let blog = document.getElementById('blog');
-let about = document.getElementById('about');
+    };
 
-let navBlog = document.getElementById('navblog');
-let navAbout = document.getElementById('navabout');
-let navProjects = document.getElementById('navprojects');
+    img3.onclick = function () {
+        imageZoom3.style.display = "block";
+        zoomContent3.src = this.src;
+    };
 
-navProjects.onclick = function(){
-    navProjects.className = "active";
-    navBlog.classList.remove("active");
-    navAbout.classList.remove("active");
-};
+    imageZoom3.onclick = function () {
+        imageZoom3.classList.add('fadeaway');
+        imageZoom3.addEventListener("animationend", listener, false);
 
-var nav = 0;
+    };
 
-navBlog.onclick = function(){
-    navBlog.className = "active";
-    navAbout.classList.remove("active");
-    navProjects.classList.remove("active");
-    about.classList.add('fadeaway');
-    about.addEventListener("animationend", abouteventlistener, false);
-};
-navAbout.onclick = function(){
-    navAbout.className = "active";
-    navBlog.classList.remove("active");
-    navProjects.classList.remove("active");
-    blog.classList.add('fadeaway');
-    blog.addEventListener("animationend", blogeventlistener, false);
-};
-
-function blogeventlistener(event) {
-    if (event.type === "animationend"){
-        switch(nav) {
-            case 0:
-                blog.style.display = "none";
-                blog.classList.remove('fadeaway');
-                about.style.display = "block";
-                about.classList.add('fadein');
-                about.addEventListener("animationend", abouteventlistener, false);
-                blog.removeEventListener("animationend", blogeventlistener, false);
-                break;
-            case 1:
-                blog.classList.remove('fadein');
-                blog.removeEventListener("animationend", blogeventlistener, false);
-                nav =  0;
-                break;
+    function listener(event) {
+        if (event.type === "animationend") {
+            imageZoom1.style.display = "none";
+            imageZoom1.classList.remove('fadeaway');
+            imageZoom1.removeEventListener("animationend", listener, false);
+            imageZoom2.style.display = "none";
+            imageZoom2.classList.remove('fadeaway');
+            imageZoom2.removeEventListener("animationend", listener, false);
+            imageZoom3.style.display = "none";
+            imageZoom3.classList.remove('fadeaway');
+            imageZoom3.removeEventListener("animationend", listener, false);
         }
     }
 }
-function abouteventlistener(event) {
-    if (event.type === "animationend"){
-        switch (nav) {
-            case 0:
-                about.classList.remove('fadein');
-                about.removeEventListener("animationend", abouteventlistener, false);
-                nav = 1;
-                break;
-            case 1:
-                about.style.display = "none";
-                about.classList.remove('fadeaway');
-                blog.style.display = "block";
-                blog.classList.add('fadein');
-                blog.addEventListener("animationend", blogeventlistener, false);
-                about.removeEventListener("animationend", abouteventlistener, false);
-                break;
-        }
-    }
-}
+$(function () {
+    'use strict';
+    var $body = $('html, body');
+    var $page = $('#main'),
+        options = {
+            debug: true,
+            prefetch: true,
+            cacheLength: 2,
+            onStart: {
+                duration: 250, // Duration of our animation
+                render: function ($container) {
+                    // Add your CSS animation reversing class
+                    $container.addClass('is-exiting');
+                    // Restart your animation
+                    smoothState.restartCSSAnimations();
+                    $body.animate( {'scrollTop': 0});
+                }
+            },
+            onReady: {
+                duration: 0,
+                render: function ($container, $newContent) {
+                    // Remove your CSS animation reversing class
+                    $container.removeClass('is-exiting');
+                    // Inject the new content
+                    $container.html($newContent);
+                    stupid();
+                }
+            }
+        },
+        smoothState = $page.smoothState(options).data('smoothState');
+});
